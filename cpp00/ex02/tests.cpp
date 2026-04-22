16828a456f88a39e049dff3fd2b3bb9de468b836
Arthur Menezes <arthurmcorrea282@gmail.com>
2026-04-22 13:55:22 -0300

    a

--- cpp00/ex02/tests.cpp
+++ cpp00/ex02/tests.cpp
@@ -21,7 +21,7 @@ int		main( void ) {
 
 	int	const				amounts[]	= { 42, 54, 957, 432, 1234, 0, 754, 16576 };
 	size_t const			amounts_size( sizeof(amounts) / sizeof(int) );
-	accounts_t				accounts( amounts, amounts + amounts_size ); // 1 - 8
+	accounts_t				accounts( amounts, amounts + amounts_size );
 	accounts_t::iterator	acc_begin	= accounts.begin();
 	accounts_t::iterator	acc_end		= accounts.end();
 
@@ -37,24 +37,24 @@ int		main( void ) {
 	ints_t::iterator	wit_begin	= withdrawals.begin();
 	ints_t::iterator	wit_end		= withdrawals.end();
 
-	Account::displayAccountsInfos(); // 9
-	std::for_each( acc_begin, acc_end, std::mem_fun_ref( &Account::displayStatus ) ); // 10 - 17
+	Account::displayAccountsInfos();
+	std::for_each( acc_begin, acc_end, std::mem_fun_ref( &Account::displayStatus ) );
 
 	for ( acc_int_t it( acc_begin, dep_begin );
 		  it.first != acc_end && it.second != dep_end;
 		  ++(it.first), ++(it.second) ) {
 
-		(*(it.first)).makeDeposit( *(it.second) ); // 18 - 25
+		(*(it.first)).makeDeposit( *(it.second) );
 	}
 
-	Account::displayAccountsInfos(); // 26
-	std::for_each( acc_begin, acc_end, std::mem_fun_ref( &Account::displayStatus ) );  // 27- 34
+	Account::displayAccountsInfos();
+	std::for_each( acc_begin, acc_end, std::mem_fun_ref( &Account::displayStatus ) );
 
 	for ( acc_int_t it( acc_begin, wit_begin );
 		  it.first != acc_end && it.second != wit_end;
 		  ++(it.first), ++(it.second) ) {
 
-		(*(it.first)).makeWithdrawal( *(it.second) ); // 35 - 42
+		(*(it.first)).makeWithdrawal( *(it.second) );
 	}
 
 	Account::displayAccountsInfos();
