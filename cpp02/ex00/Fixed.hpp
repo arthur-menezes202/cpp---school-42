289fa3f629abea37be62130653ab3c4d7504c0a8
Arthur Menezes <arthurmcorrea282@gmail.com>
2026-04-27 17:56:39 -0300

    subindo ex01

--- cpp02/ex00/Fixed.hpp
+++ cpp02/ex00/Fixed.hpp
@@ -6,16 +6,27 @@
 /*   By: armeneze <armeneze@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/27 16:14:02 by armeneze          #+#    #+#             */
-/*   Updated: 2026/04/27 16:15:06 by armeneze         ###   ########.fr       */
+/*   Updated: 2026/04/27 17:26:24 by armeneze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FIXED_HPP
 #define FIXED_HPP
-
-class FixedPoint
+#include <iostream>
+class Fixed
 {
-	
+	private:
+		int numberFixed;
+		static const int _fractional_bits = 8;
+
+	public:
+		Fixed();
+		Fixed(const Fixed &other);
+		Fixed& operator=(const Fixed &other);
+		~Fixed();
+
+		int getRawBits( void ) const;
+		void setRawBits( int const raw );
 };
 
 #endif
---
+++ cpp02/ex00/main.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: armeneze <armeneze@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/27 16:55:57 by armeneze          #+#    #+#             */
+/*   Updated: 2026/04/27 17:18:17 by armeneze         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "Fixed.hpp"
+
+int main( void ) {
+	Fixed a;
+	Fixed b( a );
+	Fixed c;
+	c = b;
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+	return 0;
+}
